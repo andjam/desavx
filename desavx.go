c72ef572f3c5343bcc2f088d44ef614224776699
andjam <8394481+andjam@users.noreply.github.com>
2023-03-13 23:45:41 +0000

    update package doc and readme

--- desavx.go
+++ desavx.go
@@ -1,11 +1,12 @@
 //go:build amd64
 // +build amd64
 
-// Package desavx implements the Data Encryption Standard (DES) as described
-// in chapter 7.4 of Handbook of Applied Cryptography, 1997. DES proceeds in 16
-// rounds, processing 64-bit plaintext blocks into 64-bit ciphertext blocks
-// using a 56-bit key. This implementation utilises x86 AVX extensions to work
-// on multiple blocks of plaintext simultaneously.
+// Package desavx is an experimental implementation of the Data Encryption
+// Standard (DES) as described in chapter 7.4 of Handbook of Applied
+// Cryptography Menezes, van Oorschot, Vanstone, 1997. DES is a symmetric-key
+// block cipher. It proceeds in 16 rounds, processing 64-bit plaintext blocks
+// into 64-bit ciphertext blocks using a 56-bit key. This implementation uses
+// AVX extensions to work on multiple blocks of plaintext simultaneously.
 package desavx
 
 import (
